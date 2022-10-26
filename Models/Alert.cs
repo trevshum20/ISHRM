6b690359546559f020cf6fdf9cce2b04d7b511c9
Austin Caprio <austincaps98@gmail.com>
2022-10-26 14:55:11 -0600

    Alert and ProgramYear models
    Outline for models : Alert and ProgramYear

---
+++ Models/Alert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISHRM.Models
+{
+    public class Alert
+    {
+        [Key]
+        [Required]
+        public int AlertID { get; set; }
+
+        [Required]
+        public string AlertName { get; set; }
+
+        [Required]
+        public bool Completed { get; set; }
+
+        [Required]
+        public DateTime StartAlert { get; set; }
+    }
+}
---
+++ Models/ProgramYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISHRM.Models
+{
+    public class ProgramYear
+    {
+            [Key]
+            [Required]
+            public int ProgramID { get; set; }
+
+            [Required]
+            public string ProgramName { get; set; }
+    }
+}
