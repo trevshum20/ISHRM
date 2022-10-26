9b05089959d5a0ba35af6844d2161d9f75dccda6
trevshum20 <trevshum20@gmail.com>
2022-10-26 15:14:51 -0600

    added Student class

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISHRM.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int BYUID { get; set; }
+        public string NetID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool International { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string NetID { get; set; }
+        public bool NameChange { get; set; }
+        public string Notes { get; set; }
+        public string BYUName { get; set; }
+    }
+}
