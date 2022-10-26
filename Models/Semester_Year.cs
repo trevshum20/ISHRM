5495df0bf801fb5655329cfe6c68ad6cc5aaa52c
trevshum20 <trevshum20@gmail.com>
2022-10-26 14:36:22 -0600

    added Semester Year model

---
+++ Models/Semester_Year.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISHRM.Models
+{
+    public class Semester_Year
+    {
+        [Key]
+        [Required]
+        public int SemesterYearID { get; set; }
+        public string SemesterName { get; set; }
+        public int Year { get; set; }
+    }
+}
